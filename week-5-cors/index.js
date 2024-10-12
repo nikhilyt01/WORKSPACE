b6846bb9f594b7b2858080e99edb8d0de3c44085
nikhilyt01 <nikhil0808yt@gmail.com>
2024-10-12 12:37:34 +0530

    use of cors

---
+++ week-5-cors/index.js
@@ -0,0 +1,17 @@
+// write html that hits the backend server  using fetch api
+//npm install -g serve , then npx serve
+
+const express=require("express")
+const app=express();
+const cors=require("cors");
+
+app.use(cors())
+app.use(express.json())
+app.post("/sum",function(req,res){
+    const a=parseInt(req.body.a)
+    const b=parseInt(req.body.b)
+    res.json({
+        ans: a+b
+    })
+})
+app.listen(3000);
