66dd0e4c3bd5f64a97518c21b2fc522605ad03e4
nikhilyt01 <nikhil0808yt@gmail.com>
2024-10-19 18:43:59 +0530

    course routes

---
+++ week8/routes/course.js
@@ -0,0 +1,30 @@
+//const router=express.Router
+const {Router}= require("express");
+const courserouter = Router();
+const {courseModel} =require("../db");
+const {purchaseModel } =require("../db");
+const {usermiddleware}=require("../middlewares/user")
+
+courserouter.post("/purchases",usermiddleware,async function(req,res){
+    const userId = req.userid;
+    const courseId = req.body.courseId;
+    await purchaseModel.create({
+        userId,
+        courseId
+    })
+    
+    res.json({
+        msg:"you have successfully bought the course"
+    })
+})
+courserouter.get("/preview",async function(req,res){
+    const courses= await courseModel.find({}) //empty means it gives all courses
+    res.json({
+        courses
+    })
+})
+
+
+module.exports={
+    courserouter:courserouter
+}
