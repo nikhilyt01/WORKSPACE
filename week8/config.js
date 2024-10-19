94552a08882f5fb9c5834fd380d133d8ecfbcec2
nikhilyt01 <nikhil0808yt@gmail.com>
2024-10-19 18:44:38 +0530

    all pass codes

---
+++ week8/config.js
@@ -0,0 +1,9 @@
+//require('dotenv').config()
+const JWT_ADMINPASS =process.env.JWT_ADMINPASS;
+const JWT_USERPASS =process.env.JWT_USERPASS;
+
+module.exports={
+    JWT_ADMINPASS,
+    JWT_USERPASS
+
+}
