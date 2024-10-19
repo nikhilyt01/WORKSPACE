359ba129ad8bbf1a701f9f7e4201574f748aa899
nikhilyt01 <nikhil0808yt@gmail.com>
2024-10-19 18:45:47 +0530

    main index.js file

---
+++ week8/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express= require("express");
+const mongoose= require("mongoose");
+const app=express();
+
+const {courserouter} = require("./routes/course")
+const {userrouter} =require("./routes/user")
+const { adminrouter } =require("./routes/admin")
+
+//const {CreateUserRoutes}=require("./routes/user");
+
+//CreateUserRoutes(app);
+//const {purchasemodel} =require ("./db");
+
+app.use(express.json())
+
+app.use("/api/v1/user",userrouter);
+app.use("/api/v1/course",courserouter);
+app.use("/api/v1/admin",adminrouter); //1st argument is prefix
+
+//console.log(process.env.MONGO_URL);
+async function main(){
+    //dotenv HW
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(3000)
+    //console.log("listening") // if some error it won't run backend
+}
+main();
+ 
