2da4187ce87c9f362e4b2a81ecaa11e43fff81cd
nikhilyt01 <nikhil0808yt@gmail.com>
2024-10-26 13:59:54 +0530

    notification icon

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -1,48 +1,25 @@
 import React , {useState} from 'react';
 import { PostComponent} from "./post" ;
 function App() {
-  const [posts,setPosts]=useState([]);
+  const [count,setCount]=useState(0);
+  function increment(){
+    setCount(count+1)
 
-  const postcomponents = posts.map(post => <PostComponent
-    name= {post.name}
-    subtitle = {post.subtitle}
-    time={post.time}
-    image={post.image}
-    description={post.description}
-  />)
-
-  function addpost(){
-
-    setPosts([...posts,{
-      name:"harkirat",
-      subtitle:"1000 followers",
-      time:"2m ago",
-      image:"https://cdn-icons-png.flaticon.com/128/17734/17734816.png",
-      description:"what to know how to earn money? play satta 24/7 "
-    }])
   }
-
-  return (
-     <div style={ {backgroundColor:"#dfe6e9",height:"100vh"}}>
-      <button onClick={addpost}> Add post</button>
-
-      <div style={{display:"flex",justifyContent:"center"}}>
-        <div>
-          
-           { postcomponents }
-          
-        </div>
-   
-        
-      </div>
-      
-     
+  return <div >
+    {/* coz span is not allowing width n height adjusts */}
+    <div style={{display:'flex'}}> 
+       <div style={{backgroundColor:"red",borderRadius:20,width:20,height:25,paddingLeft:10,paddingTop:7}}>
+          {count}
+       </div>
     </div>
-    
-
-
-    
-  )
+    <img style={ {cursor:"pointer"}} 
+    src={"https://cdn-icons-png.flaticon.com/128/1827/1827422.png"} width={50}
+    /> 
+    <button onClick={increment}>increase count</button>
+  </div>
+     
+   
 }
 
 
