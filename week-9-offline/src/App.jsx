30912d703b96cf6a00b4b47c22dd3854fc81b3f4
nikhilyt01 <nikhil0808yt@gmail.com>
2024-10-26 22:00:11 +0530

    use of useeffect

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -1,23 +1,32 @@
-import React , {useState} from 'react';
+import  {useState,useEffect} from 'react';
 import { PostComponent} from "./post" ;
 function App() {
-  const [count,setCount]=useState(0);
-  function increment(){
-    setCount(count+1)
+const [count,setCount]=useState(1);
+const [count1,setCount1]=useState(1);
 
-  }
-  return <div >
-    {/* coz span is not allowing width n height adjusts */}
-    <div style={{display:'flex'}}> 
-       <div style={{backgroundColor:"red",borderRadius:20,width:20,height:25,paddingLeft:10,paddingTop:7}}>
-          {count}
-       </div>
-    </div>
-    <img style={ {cursor:"pointer"}} 
-    src={"https://cdn-icons-png.flaticon.com/128/1827/1827422.png"} width={50}
-    /> 
-    <button onClick={increment}>increase count</button>
-  </div>
+function increasecount(){
+  setCount(function(currval){
+    return currval+1;
+  });
+  
+}
+function decreasecount(){
+  setCount1(function(currval){
+    return currval-1;
+  })
+}
+useEffect(function(){
+  console.log("above setinterval")
+  setInterval(increasecount,1000);
+  setInterval(decreasecount,1000);
+},[])
+useEffect(function(){
+  console.log("count is updated")
+},[count,count1])
+
+return <div>
+  {count} {count1}
+</div>
      
    
 }
