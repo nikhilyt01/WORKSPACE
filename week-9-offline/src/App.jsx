27a82e0f00fa342319270c575f873faa9dbef748
nikhilyt01 <nikhil0808yt@gmail.com>
2024-10-27 22:46:15 +0530

    use of tabs &fetch

--- week-9-offline/src/App.jsx
+++ week-9-offline/src/App.jsx
@@ -1,37 +1,46 @@
 import  {useState,useEffect} from 'react';
 import { PostComponent} from "./post" ;
 function App() {
-const [count,setCount]=useState(1);
-const [count1,setCount1]=useState(1);
+  const [currentTab,setCurrentTab]=useState("1");
+  const [tabData,setTabData]=useState({});
+  const [loading,setLoading]=useState(true)
+  useEffect(function(){
+    setLoading(true);
+    console.log("send req to backend to get datafrom "+currentTab)
+    fetch("https://jsonplaceholder.typicode.com/todos/"+currentTab)
+    .then(async res =>{
+      const json=await res.json();
+      setTabData(json);
+      setLoading(false)
+    })
 
-function increasecount(){
-  setCount(function(currval){
-    return currval+1;
-  });
-  
-}
-function decreasecount(){
-  setCount1(function(currval){
-    return currval-1;
-  })
-}
-useEffect(function(){
-  console.log("above setinterval")
-  setInterval(increasecount,1000);
-  setInterval(decreasecount,1000);
-},[])
-useEffect(function(){
-  console.log("count is updated")
-},[count,count1])
+  },[currentTab])
+  return <div>
+    <button onClick={ function(){
+      setCurrentTab("1")
+      } }style={ {color:currentTab=="1"?"red":"black"}}>todo#1</button>
 
-return <div>
-  {count} {count1}
-</div>
+    <button onClick={ function(){
+      setCurrentTab("2")
+      } }style={ { color: currentTab == "2"?"red":"black"}}>todo#2</button>
+
+    <button onClick={ function(){
+      setCurrentTab("3")
+      } }style={{color:currentTab=="3"?"red":"black"}}>todo#3</button>
+
+    <button onClick={function(){
+      setCurrentTab("4")
+      } }style={{color:currentTab=="4"?"red":"black"}}>todo#4</button>
+      <br></br>
+      {loading?"loading ...": tabData.title }
+   
+  </div>
+
+
+
      
    
 }
-
-
 
 
 export default App
