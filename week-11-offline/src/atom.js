e7ec708dc5eb3ee74bba9473b88aaa8b6d3a7581
nikhilyt01 <nikhil0808yt@gmail.com>
2024-11-04 14:33:39 +0530

    cleared

--- week-11-offline/src/atom.js
+++ week-11-offline/src/atom.js
@@ -1,16 +1,2 @@
-import {atomFamily} from "recoil";
-import {todos} from "./todos";
-
-export const todosAtomFamily=atomFamily({
-    key:"todosAtomFamily",
-    default:(id) =>{
-        return todos.find((x) => x.id==id)
-    },
-
-});
-
-
-
-
 
 
