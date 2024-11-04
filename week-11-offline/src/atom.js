2851c08baee6ebec91e3613832aeda6ca42c4b3f
nikhilyt01 <nikhil0808yt@gmail.com>
2024-11-04 12:57:42 +0530

    fetching from B.E

--- week-11-offline/src/atom.js
+++ week-11-offline/src/atom.js
@@ -1,30 +1,25 @@
-import {atom,selector} from "recoil"
+import { atom, selector } from "recoil";
+import axios from "axios";
 
-export const networkAtom=atom({
-    key:"networkAtom",
-    default:102
- })
- export const jobsAtom=atom({
-    key:"jobsAtom",
-    default:0
- })
- export const messagingAtom=atom({
-    key:"messagingAtom",
-    default:0
- })
-export const NotificationAtom=atom({
-    key:"notificationAtom",
-    default:12
- })
+export const notifications = atom({
+    key: "networkAtom",
+    default: selector({
+        key:"networkAtomselector",
+        get:async () => {
+            const res=await axios.get("https://sum-server.100xdevs.com/notifications")
+            return res.data
+        }
+    })
+        
+});
 
- export const Totalpings=selector({
-    key:"Totalpings",
-    get:({get}) =>{
-        const networkpings=get(networkAtom);
-        const jobspings=get(jobsAtom);
-        const messagingpings=get(messagingAtom);
-        const notificationpings=get(NotificationAtom)
-        return networkpings+jobspings+messagingpings+notificationpings
+export const totalNotificationSelector = selector({
+    key: "totalNotificationSelector",
+    get: ({get}) => {
+        const allNotifications = get(notifications);
+        return allNotifications.network + 
+        allNotifications.jobs + 
+        allNotifications.notifications + 
+        allNotifications.messaging
     }
-    
- })
+})
