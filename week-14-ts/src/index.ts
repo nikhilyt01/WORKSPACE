e0c934393bb82c4b67425854ab1f7c0457d8f6ba
nikhilyt01 <nikhil0808yt@gmail.com>
2024-12-02 23:14:53 +0530

    using interface

--- week-14-ts/src/index.ts
+++ week-14-ts/src/index.ts
@@ -1,19 +1,30 @@
 
 interface user { name:string;
     age:number;
-    address:{ country:string;
+    address?:{ country:string;
               place:string;
               pincode:number;
     };
 }
- 
-let user:user ={
+ // interface can also have interace
+let user1:user ={
     name:"nikhil",
     age:18,
-    address:{
+    address :{
         country:"sjbad",
         place:"sdf",
         pincode:82378
     }
 
 }
+function Islegal(user:user):boolean {
+    if(user.age>=18){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+ 
+let ans=Islegal(user1);
+console.log(ans);
