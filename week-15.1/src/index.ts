027feffbcead82f51c8aae4eaff6fefacda594cc
nikhilyt01 <nikhil0808yt@gmail.com>
2025-01-10 18:07:35 +0530

    basic crud of brainly

--- week-15.1/src/index.ts
+++ week-15.1/src/index.ts
@@ -2,12 +2,16 @@ import express from "express";
 import mongoose from "mongoose" ; 
 import jwt from "jsonwebtoken"
 import { usermodel } from "./db";
+import { contentmodel } from "./db";
+import { JWT_SCERET } from "./config";
+import { userMiddleware } from "./middleware";
+
 
 const app=express()
 app.use(express.json());
 
 app.post("/api/v1/signup",async (req,res) => {
-    //todo zod
+    //todo zod /// hash pasword
     const username = req.body.username;
     const password = req.body.password;
    try{
@@ -26,19 +30,59 @@ app.post("/api/v1/signup",async (req,res) => {
 
 })
 
-app.post("api/v1/signin",(req,res) => {
+app.post("/api/v1/signin",async (req,res) => {
+    const username =req.body.username;
+    const password = req.body.password;
+    const existinguser= await usermodel.findOne({
+        username,
+        password
+    })
+    if(existinguser){
+        const token=jwt.sign({
+            id:existinguser._id
+        },JWT_SCERET)
+
+        res.json({
+            token
+        })
+    }
+    else{
+        res.status(403).json({msg:"wrong credential"})
+    }
+  
 
 })
 
-app.post("api/v1/brain/share",(req,res) => {
+app.post("/api/v1/brain/share",(req,res) => {
 
 })
 
-app.post("api/v1/content",(req,res) => {
-
+app.post("/api/v1/content",userMiddleware,async (req,res) => {
+    const link=req.body.link;
+    const type=req.body.type;
+    await contentmodel.create({
+        link,
+        type,
+        //@ts-ignore
+        userId:req.userId,
+        tags:[]
+    })
+    res.json({
+        msg:"content added"
+    })
 })
 
-app.get("api/v1/content",(req,res) => {
+app.get("/api/v1/content",userMiddleware,async (req,res) => {
+    //@ts-ignore
+    const userId=req.userId;
+    const content= await contentmodel.find({
+        userId:userId
+    }).populate("userId","username")
+    res.json({content})
+
+
+})
+app.get("/api/v1/brain/:sharelink",(req,res)=>{
 
 })
 
