539df4ddc96bfc12ae26b02446fc591ab62f9d62
nikhilyt01 <nikhil0808yt@gmail.com>
2025-01-17 21:48:44 +0530

    button component

---
+++ (15.2)barily-frontend/src/components/UI/Button.tsx
@@ -0,0 +1,44 @@
+import { ReactElement } from "react";
+
+ interface Buttonprops{
+    variate:"primary" | "secondary";
+    size:"sm" | "md" | "lg";
+    text : string;
+    startIcon?: ReactElement;
+    endIcon ?:ReactElement;
+    onClick?: () => void;
+ }
+
+ const variateStyles ={
+   "primary":"bg-purple-600 text-white ", 
+   "secondary":"bg-purple-300 text-purple-600"
+ }
+ const defaultstyles="rounded-xl p-4 font-light flex items-center";
+ const sizestyle={
+   "sm":"py-1 px-2",
+   "md":"py-2 px-4",
+   "lg":"py-2 px-6"
+ }
+
+ export const Button = (props:Buttonprops) =>{
+    return <button onClick={props.onClick} className={`${variateStyles[props.variate]}  
+    ${defaultstyles} ${sizestyle[props.size]}`} > 
+
+      {props.startIcon && <div className="pr-2">{props.startIcon}</div>}
+      <span>{props.text}</span>
+      {props.endIcon && <div className="pl-2">{props.endIcon}</div>}
+       
+    </button>
+ }
+
+//  <Button variate="primary" size="sm" onClick={()=> {}} 
+//  text={"share"} ></Button>
+
+
+
+
+
+
+
+
+
