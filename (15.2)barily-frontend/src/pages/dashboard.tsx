efee733f5be7b2ec4de98ddc464e9dcd062f57f1
nikhilyt01 <nikhil0808yt@gmail.com>
2025-01-22 16:06:18 +0530

    main dashboard to simplify app.tsx

---
+++ (15.2)barily-frontend/src/pages/dashboard.tsx
@@ -0,0 +1,61 @@
+
+
+import { Button } from '../components/UI/Button';
+import { Plusicon } from '../icons/plusicons';
+import {ShareIcon} from "../icons/shareicon";
+import { Card } from '../components/UI/card';
+import { CreateContentModal } from '../components/UI/createcontentmodal';
+import {useState} from "react"
+import { Sidebar } from '../components/UI/sidebar';
+import { useContents } from '../hooks/useContents';
+import axios from "axios"
+import { Backend_url } from '../config';
+
+export function Dashboard() {
+  const [modalopen,setModalopen] =useState(false);
+  const contents =useContents();
+  
+
+//className={"py-1 pr-4 flex justify-end"}
+  return (<div>
+         <Sidebar />
+     <div className="p-4 ml-72 bg-gray-100 min-h-screen border-2"> 
+     
+       <CreateContentModal open={modalopen} onClose={()=>setModalopen(false)} />
+       <div className={"flex justify-end gap-4"}>
+          <Button 
+             onClick={()=>setModalopen(true)}
+             startIcon={<Plusicon size="lg"/>} 
+             size="md" variate="primary" 
+             text="Add content"
+             >
+          </Button>
+          <Button onClick={async ()=>{
+                const response = await axios.post(`${Backend_url}/api/v1/brain/share`,{
+                  share:true
+                 },{
+                  headers:{
+                    "Authorization": localStorage.getItem("token")
+                  }
+                 })
+                 const shareurl=`http://localhost:5173/api/v1/brain/${response.data.hash}`;
+                 alert(shareurl)
+
+          }}
+          startIcon={<ShareIcon size="lg"/>} size="md" variate="secondary" text="Share brain"></Button>
+       </div>
+      <div className="flex gap-4 flex-wrap">
+         {contents.map(  ({type,link ,title})  => <Card 
+                link={link}
+                title={title}
+                type={type} 
+          /> ) } 
+
+          
+           
+      </div>
+      
+    </div>
+   
+</div> )
+}
