b99b7327023ad933cf36d72ecfd5dd3b491ef0cd
nikhilyt01 <nikhil0808yt@gmail.com>
2025-01-24 19:41:29 +0530

    postgress use in B.E of signup

--- week-17-pg/src/index.ts
+++ week-17-pg/src/index.ts
@@ -1,17 +1,28 @@
 import {Client} from "pg";
 import express from "express";
 
-const pgClient=new Client("postgresql://neondb_owner:npg_c9HwoA6OZIYP@ep-flat-grass-a8shxklh-pooler.eastus2.azure.neon.tech/neondb?sslmode=require")
+const app=express()
+const pgClient = new Client ("postgresql://neondb_owner:npg_c9HwoA6OZIYP@ep-flat-grass-a8shxklh-pooler.eastus2.azure.neon.tech/neondb?sslmode=require")
 
-async function main(){
-    await pgClient.connect();
-    const response = await pgClient.query("SELECT email FROM users ;")
-    console.log(response.rows);
+pgClient.connect()
+app.use(express.json())
+app.post("/signup",async (req,res) =>{
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+   try{
+    const insertquery= `INSERT INTO users(username,email,password) VALUES ( '${username}' ,'${email}' ,'${password}'  );` 
+    const response = await pgClient.query(insertquery);
 
+    res.json({
+        msg : "you have signed up"
+    })
 
+}catch(e){
+    res.json({
+        msg:"error while signup"
+    })
 }
-main();
 
-
-
-
+})
+app.listen(3000);
