de720b11bbadc330b5a7f7889a379f8b5b01d49b
nikhilyt01 <nikhil0808yt@gmail.com>
2025-01-25 12:22:03 +0530

    uses JOIN to overcome boguus query

--- week-17-pg/src/index.ts
+++ week-17-pg/src/index.ts
@@ -59,5 +59,16 @@ app.get("/metadata",async (req,res)=>{
     })
 })
 
+app.get("/better-metadata",async(req,res)=>{
+    const id=req.query.id;
+    const query=`SELECT users.id, users.username, users.email, addresses.city, addresses.country, addresses.street, addresses.pincode
+               FROM users
+               JOIN addresses ON users.id = addresses.user_id
+               WHERE users.id = $1;`
 
+    const response = await pgClient.query(query,[id]);
+    res.json({
+        data: response.rows[0]
+    })
+})
 app.listen(3000);
