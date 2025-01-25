d2df3b74ac0387c943d921cbf2a0065efda073db
nikhilyt01 <nikhil0808yt@gmail.com>
2025-01-25 11:10:48 +0530

    to table with reference

--- week-17-pg/src/index.ts
+++ week-17-pg/src/index.ts
@@ -10,10 +10,22 @@ app.post("/signup",async (req,res) =>{
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
-   try{
-    const insertquery= `INSERT INTO users(username,email,password) VALUES ( $1 ,$2 ,$3  );` 
-    const response = await pgClient.query(insertquery,[username,email,password]);
 
+    const city =req.body.city;
+    const country = req.body.country;
+    const street = req.body.street;
+    const pincode=req.body.pincode;
+
+
+   try{
+    const insertquery= `INSERT INTO users(username,email,password) VALUES ( $1 ,$2 ,$3  ) RETURNING id;` 
+    const response = await pgClient.query(insertquery,[username,email,password]);
+    
+    const userid= response.rows[0].id;
+
+    const addressinsert =`INSERT INTO addresses (city,country,street,pincode,user_id) VALUES($1,$2,$3,$4,$5) ;`
+    const addressesquery = await pgClient.query(addressinsert,[city,country,street,pincode,userid])
+    
     res.json({
         msg : "you have signed up"
     })
