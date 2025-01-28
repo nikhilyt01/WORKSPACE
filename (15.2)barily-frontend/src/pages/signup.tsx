043a9ffc22a0ad78cfd421d3c8d1bb3baa8afbbc
nikhilyt01 <nikhil0808yt@gmail.com>
2025-01-29 00:12:02 +0530

    input validation

--- (15.2)barily-frontend/src/pages/signup.tsx
+++ (15.2)barily-frontend/src/pages/signup.tsx
@@ -1,4 +1,4 @@
-import { InputBox } from "../components/UI/inputbox";
+import React from "react";
 import { Button } from "../components/UI/Button";
 import { Backend_url } from "../config";
 import {useRef} from "react";
@@ -8,38 +8,58 @@ import { useNavigate } from "react-router-dom";
 
 
 export function Signup(){
-    const UsernameRef= useRef<HTMLInputElement>();
-    const PasswordRef = useRef<HTMLInputElement> ();
+    const UsernameRef= useRef<HTMLInputElement | null>(null);
+    const PasswordRef = useRef<HTMLInputElement | null> (null);
     const navigate = useNavigate();
 
     const [loading,setLoading] = useState(false);
 
-   async  function  signup(){
+   async  function  handlesignup(e:React.FormEvent<HTMLFormElement>){
+    e.preventDefault();
        setLoading(true)
        const username= UsernameRef.current?.value;
        const password = PasswordRef.current?.value;
-       try{ await axios.post(Backend_url+"/api/v1/signup",{
+       try{ 
+        const response=await axios.post(Backend_url+"/api/v1/signup",{
         username,
         password
        })
-        alert ("you have signed in ")
-      }catch(e){
-        console.error("signup error:",e)
+        alert (response.data.message)
+        navigate("/signin");
+      }catch(e:any){
+       const error= e as {response ? :{data? :{message?:string}}};
+       const errormessage= error.response?.data?.message || "signup failed"
+       alert(errormessage)
+
         
       }finally{
         setLoading(false);
-        navigate("/signin");
+        
       }
     }
 
-    return <div className={"h-screen w-screen bg-gray-200 flex justify-center items-center"}>
-                <div className={"bg-white rounded-xl border min-w-48 p-8 "}>
-                    <InputBox reference={UsernameRef} placeholder={"username"}/>
-                    <InputBox reference={PasswordRef} placeholder={"password"} />
-                    <div className={"flex justify-center pt-4"}>
-                    <Button onClick={signup} variate="primary" size="md" 
-                    loading={loading} text={"Signup"} fullwidth={true} />
+
+    return <div className={"h-screen w-screen bg-zinc-900 flex justify-center items-center"}>
+                <div className={"bg-zinc-500 rounded-xl border min-w-48 p-8 "}>
+                  <h2 className="text-2xl text-center font-bold m-6">SignUp</h2>
+                  <form onSubmit={handlesignup}>
+                    <div>
+                      <label htmlFor="username" className="block font-sm ">username</label>
+                      <input id="username"type="text" ref={UsernameRef} required className="w-full px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500"></input>
                     </div>
+                    <div>
+                      <label htmlFor="password" className="block font-sm ">password</label>
+                      <input  minLength={7} id="password" type="password" ref={PasswordRef} required  className="focus:outline-none focus:ring-2  focus:ring-blue-500 w-full px-2 py-1 rounded-1 outline  mb-2"></input>
+                    </div>
+                    <div>
+                      <Button  type="submit" loading={loading} text={loading?"Signing up..":"Sign up"}  variate="primary" size="md"fullwidth={true}  ></Button>
+                    </div>
+                  
+                     <p className="text-sm text-center mt-4">
+                      Already have account?
+                      <a href="/signin" className="text-blue-900  hover:underline"> Log in </a>
+                     </p>
+                  </form>
                 </div>
     </div>
 }
