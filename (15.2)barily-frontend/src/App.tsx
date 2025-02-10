10b2fb36cafd0a5985c169e4c9f8410189849bfc
nikhilyt01 <nikhil0808yt@gmail.com>
2025-02-10 23:01:42 +0530

    added landing pg route

--- (15.2)barily-frontend/src/App.tsx
+++ (15.2)barily-frontend/src/App.tsx
@@ -3,7 +3,9 @@ import './App.css'
 import {Dashboard} from "./pages/dashboard"
 import { Signup } from './pages/signup'
 import { Signin } from './pages/signin'
+import { LandingPage } from './pages/home'
 import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { Landingpg } from './landingpage/landing'
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
   return (
     <BrowserRouter >
         <Routes>
+            <Route  path="/test" element={<Landingpg/>}></Route>
+            <Route path="/" element={<LandingPage/>}></Route>
             <Route path="/signup" element={<Signup/>}></Route>
             <Route path="/signin" element={<Signin />}></Route>
             <Route path="/dashboard" element={<Dashboard />}></Route>
