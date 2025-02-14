5b1196dff0880529e0894874d808ddd62047c96d
nikhilyt01 <nikhil0808yt@gmail.com>
2025-02-15 01:02:06 +0530

    css

--- (15.2)barily-frontend/src/components/UI/inputbox.tsx
+++ (15.2)barily-frontend/src/components/UI/inputbox.tsx
@@ -6,6 +6,6 @@ interface InputProps {
 export function InputBox({reference,placeholder}:InputProps){
     return <div>
         <input  ref={reference} placeholder={placeholder} type={"text"} 
-        className={"px-4 py-2 border rounded m-2 bg-zinc-600  focus:outline-none focus:ring-2 focus:ring-blue-500 "} ></input>
+        className={"px-4 py-2 border rounded m-2 bg-zinc-600 text-white  focus:outline-none focus:ring-2 focus:ring-blue-500 "} ></input>
     </div>
 }
