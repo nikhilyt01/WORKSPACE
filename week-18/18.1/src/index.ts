d422384fc5f0a7bf29e0c4649ba6b57b039b4af7
nikhilyt01 <nikhil0808yt@gmail.com>
2025-02-21 12:24:20 +0530

    expressfied

--- week-18/18.1/src/index.ts
+++ week-18/18.1/src/index.ts
@@ -1,23 +1,28 @@
-
+import express from "express";
 import { PrismaClient } from "@prisma/client";
 
 const client =new PrismaClient();
+const app = express()
 
-async function CreateClient(){
-   const find=await  client.user.findFirst({
-    where:{
-        id:2
-    },
-    // select:{           only username and typescript will get automatically that it will be only string or null not the whole user
-    //     username:true
-    // }
-        
+app.get("/users", async(req,res) =>{
+    const user =await client.user.findMany();
+    res.json({user})
+})
+
+app.get("/todos/:id",async(req,res)=>{
+    const id=req.params.id;   // prisma takes params as string so used parseInt
+    const users =await client.user.findFirst({
+        where:{
+            id:parseInt(id)
+        },
+        select:{
+            todos:true
+        }
     })
-    console.log(find);
-      
-    
-}
-CreateClient();
+    res.json({users})
+})
+
+app.listen(3000);
 
 //to create 
 // await  client.user.create({
@@ -29,3 +34,6 @@ CreateClient();
 
 //     }
 // })
+
+//select:{       this will only give username but include will include relationship field
+//    username:true}
