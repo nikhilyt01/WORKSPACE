b9f06b1f5cf09449be110e30e35632fa54be91e2
nikhilyt01 <nikhil0808yt@gmail.com>
2025-02-21 11:50:57 +0530

    cleint.findfirst

--- week-18/18.1/src/index.ts
+++ week-18/18.1/src/index.ts
@@ -4,20 +4,28 @@ import { PrismaClient } from "@prisma/client";
 const client =new PrismaClient();
 
 async function CreateClient(){
-  await  client.user.create({
-    data:{
-                username:"nikhil",
-                password:"12332",
-               age:21,
-                city:"aligarh"
-    
-             }
-   
+   const find=await  client.user.findFirst({
+    where:{
+        id:2
+    },
+    // select:{           only username and typescript will get automatically that it will be only string or null not the whole user
+    //     username:true
+    // }
         
     })
-   
+    console.log(find);
       
     
 }
 CreateClient();
 
+//to create 
+// await  client.user.create({
+//     data:{
+//         username:"nikhil",
+//         password:"12332",
+//         age:21,
+//         city:"aligarh"
+
+//     }
+// })
