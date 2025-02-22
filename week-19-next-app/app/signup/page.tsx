08e899f972e501e822d2f582dadcb01a7b449711
nikhilyt01 <nikhil0808yt@gmail.com>
2025-02-23 02:16:17 +0530

    signup

---
+++ week-19-next-app/app/signup/page.tsx
@@ -0,0 +1,34 @@
+"use client"      // whenever we use onClick functionality this is must 
+
+import { useState } from "react";
+import axios from  "axios";
+import { useRouter } from "next/navigation";
+
+    
+export default function Signin(){
+    const router = useRouter();
+    const [username,setUsername] =useState("");
+    const [password,setPassword]=useState("")
+    return( <div className="w-screen h-screen flex justify-center items-center ">
+                <div className="border p-2  ">
+                    <div className="mb-2">
+                    <input className="text-gray-700" type="text" placeholder="username" onChange={e => {setUsername(e.target.value)}}></input>
+                    </div>
+
+                    <div className="mb-2">
+                    <input className="text-gray-700" type="text" placeholder="password" onChange={e => {setPassword(e.target.value)}}></input>
+                    </div>
+
+                    <button  className="rounded bg-blue-500 px-2 py-1 " onClick={async()=> {
+                       await axios.post("http://localhost:3000/api/v1/signup",{
+                            username,
+                            password
+                        })
+                        router.push("/signin")
+                    }}> signup</button>
+
+                </div>
+
+           </div>)
+
+}
