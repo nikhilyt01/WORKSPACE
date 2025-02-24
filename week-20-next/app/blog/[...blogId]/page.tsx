2fc181a217441d7a8a123b29c66314a89eed2829
nikhilyt01 <nikhil0808yt@gmail.com>
2025-02-24 17:44:21 +0530

    type [[]] double dynamic route logic

--- week-20-next/app/blog/[...blogId]/page.tsx
+++ week-20-next/app/blog/[...blogId]/page.tsx
@@ -6,12 +6,16 @@ export default async function Blogpage({params}:any){
 
 
     return (<div>
+        header
            Blog page number{JSON.stringify(BlogId)}
-
+        footer
            {/* Catch all segments blogId */}
            
            {/* => stringfy to  store array  if folder name [...blogid] it will store every thing after blogid in array  */}
               {/* if user is at /blog then also it will catch */}
+
+
+    {/* when we rename parent folder to [ [...blogId]] it can handle /blog also i.e if random routes Json.stringfy stores array else at /blog only header n footer */}
               
 
            </div>)
