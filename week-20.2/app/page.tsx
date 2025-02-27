677a084e9887755195cc4c24d79587b323788ba2
nikhilyt01 <nikhil0808yt@gmail.com>
2025-02-27 16:11:52 +0530

    getServerSession  for SSR

--- week-20.2/app/page.tsx
+++ week-20.2/app/page.tsx
@@ -1,24 +1,13 @@
-"use client"
 
+
+import { getServerSession } from "next-auth";
 import { signOut,signIn, SessionProvider, useSession } from "next-auth/react";
 
 
-export default function Home() {
- return (
-      <SessionProvider> 
-              <RealHome/>
-      </SessionProvider>
-
+export default async function Home() {
+  const session = await getServerSession();
+ return (<div>
+      {JSON.stringify(session)}
+   </div>
  )
-}
-
-function RealHome(){
-  const session = useSession();  // to use it we need to wrap it inside usesession provider
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-   
-      {session.status=="authenticated"&& <button onClick={()=>signOut()}>LogOut</button>}
-      {session.status=="unauthenticated"&& <button onClick={()=>signIn()}>signin</button>}
-    </div>
-  );
 }
