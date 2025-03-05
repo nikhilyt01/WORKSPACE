61d22849754d3a030210dd4895a65662154eb799
nikhilyt01 <nikhil0808yt@gmail.com>
2025-03-06 00:10:30 +0530

    testimonial section of landing pg

--- (15.2)barily-frontend/src/landingpage/components/testimonial.tsx
+++ (15.2)barily-frontend/src/landingpage/components/testimonial.tsx
@@ -6,10 +6,12 @@ export const Testimonials =()=>{
         <div>
             <h2 className="text-white text-center font-bold text-[4vw] mb-4">What Our Users Say</h2>
         </div>
-        <div className="bg-zinc-800 rounded shadow-lg max-w-2xl text-center mx-auto py-6">
-            <p className=" italic text-zinc-200">"This platform helped me ace my exams! The community is super helpful."</p>
-            <p className="font-semibold "> -Nikhil Singh</p>
-
+        <div className="flex bg-zinc-800 rounded shadow-lg max-w-2xl text-center mx-auto py-6">
+            <img src="/profileimgtag.jpg" alt="profile" className="h-12 w-12 rounded-full object-cover mr-2 ml-4"></img>
+            <div>
+                 <p className=" italic text-zinc-200">"This platform helped me ace my exams! The community is super helpful."</p>
+                 <p className="font-semibold "> -Nikhil Singh</p>
+            </div>
         </div>
     </div>
     </section>
