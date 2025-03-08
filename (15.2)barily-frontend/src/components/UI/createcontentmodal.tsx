00fbd7a6db3b38e4f3f2f3e1a47d6636c8e54069
nikhilyt01 <nikhil0808yt@gmail.com>
2025-03-09 00:56:19 +0530

    modal of Content

--- (15.2)barily-frontend/src/components/UI/createcontentmodal.tsx
+++ (15.2)barily-frontend/src/components/UI/createcontentmodal.tsx
@@ -8,11 +8,20 @@ import { Backend_url } from "../../config";
 
 enum ContentType{
    Youtube ="youtube",
-   Twitter ="twitter"
+   Twitter ="twitter",
+   Document="Document",
+   Links="Links",
+
+ }
+ interface props{
+   open:boolean;
+   onClose:()=>void;
+   refresh:()=>void;   // taken mannual-contentrefresh function and then call it afterwards
+
 
  }
 
-export function CreateContentModal({open,onClose}){
+export function CreateContentModal({open,onClose,refresh}:props){
 
    const titleRef= useRef<HTMLInputElement>();
    const linkRef=useRef<HTMLInputElement>();
@@ -31,17 +40,18 @@ export function CreateContentModal({open,onClose}){
       }})
       alert("content added")
       onClose();
+      refresh();
 
    }
 
    return <div>
   {open && <div>
-  <div className="w-screen h-screen bg-slate-500 fixed top-0 left-0 opacity-60 flex justify-center "> 
+  <div className="w-screen h-screen bg-slate-500 fixed top-0 left-0 opacity-20 flex justify-center "> 
   </div> 
 
    <div className="w-screen h-screen  fixed top-0 left-0  flex justify-center "> 
         <div className={"flex flex-col justify-center"}>
-           <span className={"bg-zinc-800 opacity-100 p-4 rounded-lg shadow-lg shadow-cyan-500/50 outline outline-gray-200 "}>
+           <span className={"text-center bg-zinc-800 opacity-100 p-4 rounded-lg shadow-lg shadow-cyan-500/50 outline outline-gray-200 "}>
                <div className={"flex justify-end"}>
                   <div onClick={onClose} className={"cursor-pointer text-red-500"}>
                      <CrossIcon />
@@ -53,12 +63,18 @@ export function CreateContentModal({open,onClose}){
               
               </div>
               <h1 className="text-xl font-semibold text-white">Type</h1>
-              <div className={"flex gap-1 p-4"}>
+              <div className={"grid grid-cols-2 gap-1 p-4"}>
                <Button text="youtube"  size="lg" variate = {type===ContentType.Youtube ?
                "primary" : "secondary"} onClick={() => {setType(ContentType.Youtube)
                }}></Button>
                <Button text="Twitter" size="lg" variate={type===ContentType.Twitter ? 
                   "primary":"secondary"} onClick={() =>{ setType(ContentType.Twitter)
+               } }></Button>
+               <Button text="Document" size="lg" variate={type===ContentType.Document ? 
+                  "primary":"secondary"} onClick={() =>{ setType(ContentType.Document)
+               } }></Button>
+                <Button text="Website" size="lg" variate={type===ContentType.Links ? 
+                  "primary":"secondary"} onClick={() =>{ setType(ContentType.Links)
                } }></Button>
               </div>
               <div className={"flex justify-center"}>
