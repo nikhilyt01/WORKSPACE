b23435a4788fe4daff6c349d12e8551c9aa3a878
nikhilyt01 <nikhil0808yt@gmail.com>
2025-03-09 00:56:44 +0530

    sidebar items

--- (15.2)barily-frontend/src/components/UI/sidebaritems.tsx
+++ (15.2)barily-frontend/src/components/UI/sidebaritems.tsx
@@ -1,19 +1,22 @@
 import {ReactElement} from "react";
 
-export function SidebarItem({text,Icon}:{
+export function SidebarItem({text,Icon,onClick,open}:{
     text:string;
     Icon:ReactElement;
+    onClick ?:() =>void;
+    open?:boolean;
 }) {  
     //  but icon has no effect in text color so we used ,fill ="current color"
-    return <div 
-    className={"flex items-center text-white text-2xl py-2 cursor-pointer hover:bg-gray-500 rounded max-w-48 pl-4 transition-all ease-in-out duration-100  "}> 
+    return <div  onClick={onClick}       // attched props of onclick which will cause to setfilter
+    className={"h-14  mb-2 flex items-center text-white text-xl md:text-2xl py-2 cursor-pointer  hover:bg-gray-500 rounded w-full pl-4 transition-all ease-in-out duration-100  "}> 
             <div className={"pr-2"}>
                 {Icon}
             </div>
-            <div >
+            {/* text bahar na jayee */}
+          {open && <div className=" overflow-hidden " >   
                 {text}
             </div>
-
+          }
     </div>
 }
  
