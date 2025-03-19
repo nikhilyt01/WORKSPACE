e5963c0b4621e76826c21348eee381754c1c55b2
nikhilyt01 <nikhil0808yt@gmail.com>
2025-03-19 14:35:35 +0530

    components in Package

---
+++ week-21-chatapp/packages/ui/src/input.tsx
@@ -0,0 +1,15 @@
+interface proptype{
+    placeholder:string;
+    size:"big"| "small";
+}
+export function TextInput({placeholder,size}:proptype){
+    
+    return <input placeholder={placeholder} style={{
+        padding: size=="big" ? 20 :10,
+        margin: size=="big" ? 20 :10,
+        borderColor:"black",
+        borderWidth:1,
+        }}>
+    </input>
+
+}
