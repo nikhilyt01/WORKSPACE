5a4643ccdb7c3cdbcdaf092d1a216cf00ecb1d15
nikhilyt01 <nikhil0808yt@gmail.com>
2025-03-24 23:36:59 +0530

    patch update

--- week-22/draw-app/apps/http-backend/src/index.ts
+++ week-22/draw-app/apps/http-backend/src/index.ts
@@ -1,4 +1,34 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import {JWT_SECRET}  from "@repo/backend-common/config"
+import { middleware } from "./middleware";
+import {CreateUserSchema} from "@repo/common/types";
+
 const app = express();
+
+app.post("/signup",(req,res) => {
+
+     const data = CreateUserSchema.safeParse(req.body);
+     if(!data.success){
+         res.json({
+               message : "Incorrect Inputs"
+          })
+          return ;
+     }
+
+})
+
+app.post("/signin",(req,res)=>{
+     const  userId=1;
+     const token = jwt.sign({userId},JWT_SECRET)
+})
+
+app.post("/room",middleware,(req,res)=>{
+     //some db call
+     res.json({
+          roomId:123
+     })
+
+})
 
 app.listen(3001);
