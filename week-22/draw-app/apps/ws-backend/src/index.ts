a423f97013a77a86879e3de4801ca263c4a51d84
nikhilyt01 <nikhil0808yt@gmail.com>
2025-04-01 23:48:20 +0530

    completed ws-route

--- week-22/draw-app/apps/ws-backend/src/index.ts
+++ week-22/draw-app/apps/ws-backend/src/index.ts
@@ -1,7 +1,21 @@
 import { WebSocketServer } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
+
 const wss =new WebSocketServer({port:8080});
+
+function checkuser(token:string):string | null {
+    const decoded=jwt.verify(token,JWT_SECRET);
+    if(typeof decoded=="string"){
+       wss.close();
+       return null;
+    }
+    if(!decoded || !decoded.userId){
+        return null;
+    }
+    return decoded.userId
+
+}
 
 wss.on("connection",function connection(ws,request) {
     const url = request.url; // ws://localhost:3000?token=1243647  yaha pe split after ?
@@ -11,12 +25,12 @@ wss.on("connection",function connection(ws,request) {
     }
     const queryParams = new URLSearchParams(url.split('?')[1]);
     const token = queryParams.get('token') || "";
-    const decoded = jwt.verify(token,JWT_SECRET);
 
-    if(!decoded || !(decoded as JwtPayload).userId){
+    const userId=checkuser(token);
+    if(!userId){
         ws.close();
-        return;
     }
+    
 
     ws.on("message",function message(data){
         ws.send("pong");
