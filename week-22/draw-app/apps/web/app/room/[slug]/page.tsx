c99107065185c39942c5d2593a1eb18c971fc1fb
nikhilyt01 <nikhil0808yt@gmail.com>
2025-04-08 15:24:25 +0530

    catching /room/:slug

--- week-22/draw-app/apps/web/app/room/[slug]/page.tsx
+++ week-22/draw-app/apps/web/app/room/[slug]/page.tsx
@@ -1,1 +1,22 @@
 import axios from "axios"
+import { Backend_url } from "../../config"
+
+async function getRoom(slug:string){
+   const response=await  axios.get(`${Backend_url}/room/${slug}`)
+   return response.data.id;
+}
+
+interface paramss{
+    slug:string
+}
+export default async function ChatRoom({
+    params
+
+}:{params:{
+    slug:string
+   }
+})  {
+    const slug=params.slug;
+    const roomId=await getRoom(slug)
+
+}
