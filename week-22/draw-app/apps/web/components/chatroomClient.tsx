ce9ce91086d87d041813e3c57b9fc52605f4a5bf
nikhilyt01 <nikhil0808yt@gmail.com>
2025-04-09 14:22:36 +0530

    chatroom-client

---
+++ week-22/draw-app/apps/web/components/chatroomClient.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+export function ChatRoomClient({
+    messages,
+    id
+}:{
+    messages:{messages:string}[],            // array of objects    // messages: [{ messages: "Hi" }, { messages: "Bye" }],
+    id:string
+}){
+
+
+
+
+    
+}
