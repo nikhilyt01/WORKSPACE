bf342a24200e09653fd4a5192d158eb98f83969a
nikhilyt01 <nikhil0808yt@gmail.com>
2025-04-10 23:59:59 +0530

    updated

--- week-22/draw-app/apps/web/app/room/[slug]/page.tsx
+++ week-22/draw-app/apps/web/app/room/[slug]/page.tsx
@@ -1,22 +1,23 @@
 import axios from "axios"
 import { Backend_url } from "../../config"
+import { ChatRoom } from "../../../components/chatroom";
 
 async function getRoom(slug:string){
    const response=await  axios.get(`${Backend_url}/room/${slug}`)
-   return response.data.id;
+   return response.data.room.id;  
 }
 
-interface paramss{
-    slug:string
-}
-export default async function ChatRoom({
+
+export default async function ChatRoom1({
     params
-
-}:{params:{
-    slug:string
+}:{
+    params: {
+        slug:string
    }
 })  {
-    const slug=params.slug;
+    const slug=(await params).slug;
     const roomId=await getRoom(slug)
+
+    return <ChatRoom id={roomId} ></ChatRoom>
 
 }
