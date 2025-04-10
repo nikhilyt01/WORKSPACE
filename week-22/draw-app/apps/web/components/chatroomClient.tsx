7bcd885de3362c4c4b574b1ede2fcc27a175303f
nikhilyt01 <nikhil0808yt@gmail.com>
2025-04-10 23:59:39 +0530

    client part updated

--- week-22/draw-app/apps/web/components/chatroomClient.tsx
+++ week-22/draw-app/apps/web/components/chatroomClient.tsx
@@ -1,14 +1,52 @@
 "use client"
 
+import { useEffect, useState } from "react";
+import{useSockets} from "../hooks/useSockets"
+
 export function ChatRoomClient({
     messages,
     id
 }:{
-    messages:{messages:string}[],            // array of objects    // messages: [{ messages: "Hi" }, { messages: "Bye" }],
+    messages:{message:string}[],          // array of objects    // messages: [{ messages: "Hi" }, { messages: "Bye" }],
     id:string
 }){
+  const  [chats,setChats]=useState(messages);
+  const {socket,loading}= useSockets();
+  const [currentMessage, setCurrentMessage] = useState("");
 
+ useEffect(()=>{
+    if(socket && !loading){
+        socket.onmessage=(event)=>{
+            
+            socket.send(JSON.stringify({
+                type:"join_room",
+                roomId:id
+            }))
 
+            const parsedData=JSON.parse(event.data);
+            if(parsedData.type==="chat"){
+                setChats(c => [...c,{message: parsedData.message}])
+            }
+        }
+    }
+
+ },[socket,loading,id])
+
+ return <div>
+    {chats.map(m=><div>{m.message}</div>)}
+    <input type="text" value={currentMessage} onChange={e => {
+            setCurrentMessage(e.target.value);
+        }}></input>
+        <button onClick={() => {
+            socket?.send(JSON.stringify({
+                type: "chat",
+                roomId: id,
+                message: currentMessage
+            }))
+
+            setCurrentMessage("");
+        }}>Send message</button>
+    </div>
 
 
     
