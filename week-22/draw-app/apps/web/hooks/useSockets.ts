33239eb286598e6f23f41b80671f70fe8defef3c
nikhilyt01 <nikhil0808yt@gmail.com>
2025-04-11 22:53:09 +0530

    hardcoded url & token

--- week-22/draw-app/apps/web/hooks/useSockets.ts
+++ week-22/draw-app/apps/web/hooks/useSockets.ts
@@ -6,7 +6,7 @@ export function useSockets(){
     const [socket,setSocket]=useState<WebSocket>();
 
     useEffect(()=>{
-        const ws =new WebSocket(ws_url);
+        const ws =new WebSocket(`${ws_url}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI0MWEyZjcwOC0zNTQxLTRmOWItYWM5OS04YWUwYzlkNjgxMTYiLCJpYXQiOjE3NDM0NDk3NDZ9.K3xGO5v2hIfV7f7mt95fWKCiq8Rba8zFhnZB_tmO85w`);
         ws.onopen=()=>{
             setLoading(false);
             setSocket(ws);
