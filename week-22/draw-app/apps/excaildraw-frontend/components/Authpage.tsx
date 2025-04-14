3b259f03d8b600db8d57c3a8bebfd4f16ae5b601
nikhilyt01 <nikhil0808yt@gmail.com>
2025-04-14 23:09:11 +0530

    updates

--- week-22/draw-app/apps/excaildraw-frontend/components/Authpage.tsx
+++ week-22/draw-app/apps/excaildraw-frontend/components/Authpage.tsx
@@ -1,17 +1,17 @@
-"use client"
+"use client";
 
 export function  Authpage({isSignin}:{isSignin:boolean}){
 
     return <div className="w-screen h-screen flex justify-center items-center">
         <div className="p-6 m-2 bg-white rounded">
             <div className="p-2">
-               <input type="text" placeholder="Email" ></input>
+               <input type="text"  className="border border-gray-300 rounded px-3 py-2 w-full placeholder-gray-500" placeholder="Email" ></input>
             </div>
             <div className="p-2">
-                <input type="password" placeholder="password"></input>
+                <input type="password" className="border border-gray-300 rounded px-3 py-2 w-full placeholder-gray-500"  placeholder="password"></input>
             </div>
-            <div className="p-2">
-                <button onClick={()=>{
+            <div className="pt-2">
+                <button className="bg-red-300" onClick={()=>{
 
                  }}>{isSignin?"Sign in":"Sign up"}</button>
             </div>
