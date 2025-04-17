a66bca0b2696eb72f839d51a21fff581e09b43ee
nikhilyt01 <nikhil0808yt@gmail.com>
2025-04-17 23:27:01 +0530

    draw rect using drag

--- week-22/draw-app/apps/excaildraw-frontend/app/canvas/[roomId]/page.tsx
+++ week-22/draw-app/apps/excaildraw-frontend/app/canvas/[roomId]/page.tsx
@@ -9,16 +9,46 @@ export default function Canvas(){
         if(canvasRef.current){
               const canvas =canvasRef.current;
               const ctx=canvas.getContext("2d");
+
               if(!ctx){
                 return
               }
-              ctx.strokeRect(5,25,100,100)
+
+             
+
+              let clicked=false;
+              let startx=0;
+              let starty=0;
+
+             canvas.addEventListener("mousedown",(e)=>{
+              clicked=true;
+              startx= e.clientX
+              starty=e.clientY
+             })
+ 
+             canvas.addEventListener("mouseup",(e)=>{
+              clicked=false;
+              console.log(e.clientX)
+              console.log(e.clientY)
+             })
+
+             canvas.addEventListener("mousemove",(e)=>{
+              if(clicked){
+               const width = e.clientX - startx;
+               const height = e.clientY - starty;
+               ctx.clearRect(0,0,canvas.width,canvas.height);
+
+              
+               ctx.strokeRect(startx,starty,width,height);
+               
+              }
+             })
         }
 
     },[canvasRef])
 
     return <div>
-               <canvas ref={canvasRef} width={500} height={500}></canvas>
+               <canvas ref={canvasRef} width={1080} height={1000}></canvas>
         </div>
 
 }
