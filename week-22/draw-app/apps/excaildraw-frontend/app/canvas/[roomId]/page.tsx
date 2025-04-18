3a538523ae75fc87c44705c4a2337d7f095769a6
nikhilyt01 <nikhil0808yt@gmail.com>
2025-04-18 23:51:34 +0530

    simplified

--- week-22/draw-app/apps/excaildraw-frontend/app/canvas/[roomId]/page.tsx
+++ week-22/draw-app/apps/excaildraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,54 +1,26 @@
 "use client"
 
 import {useRef,useEffect} from "react"
+import { initDraw } from "@/draw";
 
 export default function Canvas(){
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(()=>{
         if(canvasRef.current){
-              const canvas =canvasRef.current;
-              const ctx=canvas.getContext("2d");
-
-              if(!ctx){
-                return
-              }
-
+             initDraw(canvasRef.current)   // canvas html ke attribute ko hi pass krdiye
+           
              
-
-              let clicked=false;
-              let startx=0;
-              let starty=0;
-
-             canvas.addEventListener("mousedown",(e)=>{
-              clicked=true;
-              startx= e.clientX
-              starty=e.clientY
-             })
- 
-             canvas.addEventListener("mouseup",(e)=>{
-              clicked=false;
-              console.log(e.clientX)
-              console.log(e.clientY)
-             })
-
-             canvas.addEventListener("mousemove",(e)=>{
-              if(clicked){
-               const width = e.clientX - startx;
-               const height = e.clientY - starty;
-               ctx.clearRect(0,0,canvas.width,canvas.height);
-
-              
-               ctx.strokeRect(startx,starty,width,height);
-               
-              }
-             })
         }
 
     },[canvasRef])
 
     return <div>
-               <canvas ref={canvasRef} width={1080} height={1000}></canvas>
+               <canvas ref={canvasRef} width={2000} height={1000}></canvas>
+               <div className="absolute bottom-0 right-0">
+                 <div className="bg-white text-black">Rect</div>
+                 <div className="bg-white text-black">Circle</div>
+               </div>
         </div>
 
 }
