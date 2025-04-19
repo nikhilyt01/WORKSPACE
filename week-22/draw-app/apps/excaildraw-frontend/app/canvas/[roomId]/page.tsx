8800006d24b12f56591990a90b4dd5978e0397bc
nikhilyt01 <nikhil0808yt@gmail.com>
2025-04-19 23:22:36 +0530

    made async fun

--- week-22/draw-app/apps/excaildraw-frontend/app/canvas/[roomId]/page.tsx
+++ week-22/draw-app/apps/excaildraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,26 +1,14 @@
-"use client"
-
 import {useRef,useEffect} from "react"
 import { initDraw } from "@/draw";
+import { RoomCanvas } from "@/components/Roomcanvas";
 
-export default function Canvas(){
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+export default async function Canvaspage({params}:{
+  params : {              // this just extracts the roomId params
+     roomId:string
+  }
+}){
+    const roomId=(await params).roomId
 
-    useEffect(()=>{
-        if(canvasRef.current){
-             initDraw(canvasRef.current)   // canvas html ke attribute ko hi pass krdiye
-           
-             
-        }
-
-    },[canvasRef])
-
-    return <div>
-               <canvas ref={canvasRef} width={2000} height={1000}></canvas>
-               <div className="absolute bottom-0 right-0">
-                 <div className="bg-white text-black">Rect</div>
-                 <div className="bg-white text-black">Circle</div>
-               </div>
-        </div>
+    return <RoomCanvas roomId={roomId}/>
 
 }
