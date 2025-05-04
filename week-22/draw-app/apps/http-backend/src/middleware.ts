0a8315ed1adc66bd81c8811541745a9808c33c2f
nikhilyt01 <nikhil0808yt@gmail.com>
2025-05-04 13:26:17 +0530

    error  handling

--- week-22/draw-app/apps/http-backend/src/middleware.ts
+++ week-22/draw-app/apps/http-backend/src/middleware.ts
@@ -3,17 +3,34 @@ import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 
 export function  middleware(req:Request,res:Response,next:NextFunction){
-    const token = req.headers["authorization"] ;
+    const token = req.headers["authorization"] ??"" ;
+    if (!token) {
+        res.status(401).json({ error: "No token provided" });
+        return; 
+    }
    
-    const decoded = jwt.verify(token as string,JWT_SECRET);
+    try{           //type safety
+    const decoded:string|JwtPayload = jwt.verify(token ,JWT_SECRET);
 
     if(decoded){
         // we will add that global req updation part
         req.userId=(decoded as JwtPayload).userId;
         next();
-
+        return;
     }else{
         res.status(403).json({message:"unauthorized"})
     }
+}catch{
+    res.status(401).json({ error: "Invalid or expired token" })
+    return;
+}
 
 }
+
+//  app.get("/test-room/:id", async (req, res) => {
+//      const room = await prismaClient.$queryRaw`
+//        SELECT * FROM "Room" WHERE id = ${Number(req.params.id)}
+//      `;
+//       res.json(room);
+//    });
+   
