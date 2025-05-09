0188a72dbea9dbd179c955df7246b809caac22cc
nikhilyt01 <nikhil0808yt@gmail.com>
2025-05-09 23:10:04 +0530

    unauth pg

---
+++ week-22/draw-app/apps/excaildraw-frontend/components/unAuthpage.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from "next/navigation"
+
+export const UnAuth = ()=>{
+   const router=useRouter()
+    return (
+        <div className="h-full w-full fixed inset-0 bg-zinc-900 flex justify-center items-center">
+        
+            <div className=" max-w-sm rounded-xl shadow-lg shadow-cyan-900/50  outline outline-1 outline-white bg-zinc-800 text-center p-4 ">
+              <h2 className="text-bold font-bold text-red-500 text-3xl mb-2"> Access Denied !</h2>
+              <p className="text-md font-semibold text-white">You Do not seems to Be Authentic user or token must be expired Try Again Later</p>
+              <div className="my-4 text-center">
+                <button onClick={()=>router.push("/")} className="rounded px-4 py-1 bg-blue-500 text-white cursor-pointer hover:bg-blue-600" >
+                     Go Home
+                </button>
+
+              </div>
+
+            </div>
+        
+
+        </div>
+    )
+}
