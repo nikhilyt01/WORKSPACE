3fb3e0a9cb2bec2fc3edcf37bef5368a0abd810a
nikhilyt01 <nikhil0808yt@gmail.com>
2025-06-14 22:53:28 +0530

    UI build

--- figma_test/ticktech_jr/packages/ui/src/button.tsx
+++ figma_test/ticktech_jr/packages/ui/src/button.tsx
@@ -18,3 +18,19 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
     </button>
   );
 };
+// better to use Ui library
+// interface buttonProps{   
+//     type:"primary" | "secondary";
+//     text:string;
+//     onClick ?:()=>void;
+//     className?: string;
+// }
+// export const Button =({type,text,onClick,className}:buttonProps)=>{
+//     return (
+//         <button className={`px-6 py-2 rounded-xl font-bold ${type==="primary"?"bg-orange-500 text-white shadow-lg ":""}
+//             ${type==="secondary"?"bg-white text-orange-500 border border-orange-500 shadow-lg ":""} ${className}`} >
+//             {text}
+//         </button>
+//     )
+
+// }
