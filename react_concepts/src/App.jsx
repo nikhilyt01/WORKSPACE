d1866ae760bf19e37101513e73af4ba848c00f95
nikhilyt01 <nikhil0808yt@gmail.com>
2025-07-06 23:39:01 +0530

    context Api bascis

--- react_concepts/src/App.jsx
+++ react_concepts/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider';
+import Login from './components/login';
+import Profile from './components/profile';
 
 
 
@@ -16,22 +19,13 @@ function App() {
 
 
   return (
-    <div className="w-screen h-screen  " style={{background:color}}>
-
-     <div className="absolute top-4 left-1/2 transform -translate-x-1/2  px-6 py-2 bg-white rounded-full flex items-center border border-blue-900 gap-4 shadow-lg">
-        <div className="flex gap-2">
-          {basicc.map( (c) => (
-            <button key={c} className="px-4 py-2 outline-none text-white text-bold rounded-full cursor-pointer hover:scale-105 active:scale-95 " style={{backgroundColor:c}} 
-            onClick={()=>setColor(c)}>{c}</button>
-
-          ))}
-
-        </div>
-      </div>
-        
+    <UserContextProvider>
      
-     
-    </div>
+        <h1 className='flex justify-center items-center '>React concepts -- prop Drilling</h1>
+        <Login />
+        <Profile />
+      
+    </UserContextProvider>
   )
 }
 
