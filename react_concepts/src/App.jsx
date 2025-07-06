f4a2e7a32b765035ed7af3d8d98fe6e26321dac7
nikhilyt01 <nikhil0808yt@gmail.com>
2025-07-06 18:13:09 +0530

    revsising bascis

--- react_concepts/src/App.jsx
+++ react_concepts/src/App.jsx
@@ -6,41 +6,32 @@ import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("red");
+  const basicc=[
+    "red",
+    "blue",
+    "yellow",
+    "green",
+  ]
 
-function reduce(){
-  setCount((count)=>{
-    if(count > 0){
-     return  count -1;
-    }
-   else{
-    return count=0;
-   }
-  })
-}
+
   return (
-    <>
-      <div>
+    <div className="w-screen h-screen  " style={{background:color}}>
+
+     <div className="absolute top-4 left-1/2 transform -translate-x-1/2  px-6 py-2 bg-white rounded-full flex items-center border border-blue-900 gap-4 shadow-lg">
+        <div className="flex gap-2">
+          {basicc.map( (c) => (
+            <button key={c} className="px-4 py-2 outline-none text-white text-bold rounded-full cursor-pointer hover:scale-105 active:scale-95 " style={{backgroundColor:c}} 
+            onClick={()=>setColor(c)}>{c}</button>
+
+          ))}
+
+        </div>
+      </div>
         
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) =>{
-          if(count==20){
-            return 20;
-          }
-          return count +1;
-        })}>
-          count is increased to :{count}
-        </button>
-        <br/>
-        <br/>
-         <button onClick={reduce}>
-          count is reduced to: {count}
-        </button>
-      </div>
      
-    </>
+     
+    </div>
   )
 }
 
