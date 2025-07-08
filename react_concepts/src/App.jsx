14500a4a928c7302f9138cb5efdb42a1964cfc4d
nikhilyt01 <nikhil0808yt@gmail.com>
2025-07-08 12:33:09 +0530

    theme switcher

--- react_concepts/src/App.jsx
+++ react_concepts/src/App.jsx
@@ -1,31 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserContextProvider from './context/UserContextProvider';
 import Login from './components/login';
 import Profile from './components/profile';
+import { ThemeProvider } from './context/theme';
+import ThemeBtn from './components/themebtn';
+import Card from './components/card';
 
 
 
 function App() {
-  const [color, setColor] = useState("red");
-  const basicc=[
-    "red",
-    "blue",
-    "yellow",
-    "green",
-  ]
+const [thememode,setThememode]=useState("light");
 
+const lighttheme=() =>{
+    setThememode("light")
+}
+const darktheme=()=>{
+    setThememode("dark")
+}
+
+useEffect(()=>{
+  document.querySelector("html").classList.remove("light","dark");
+  document.querySelector("html").classList.add(thememode)
+
+},[thememode])
 
   return (
-    <UserContextProvider>
-     
-        <h1 className='flex justify-center items-center '>React concepts -- prop Drilling</h1>
-        <Login />
-        <Profile />
-      
-    </UserContextProvider>
+<ThemeProvider value={{thememode,lighttheme,darktheme}}>
+  <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+             <ThemeBtn/>
+           </div>
+
+           <div className="w-full max-w-sm mx-auto">
+             <Card/>
+           </div>
+      </div>
+  </div>
+</ThemeProvider>
+
   )
 }
 
