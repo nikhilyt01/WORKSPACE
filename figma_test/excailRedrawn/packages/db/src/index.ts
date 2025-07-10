257aa94d28ecae07a9d7c1f5df1a0ce78ea3e228
nikhilyt01 <nikhil0808yt@gmail.com>
2025-07-10 22:54:04 +0530

    setuped Database package

---
+++ figma_test/excailRedrawn/packages/db/src/index.ts
@@ -0,0 +1,6 @@
+
+//Due to update by Prisma 4+
+//import {PrismaClient} from {"./output wala"}     // ma chudaaye update apan Purane jamne me hi  chalne akhir me 1 din bad resolve old method se hi
+import {PrismaClient}  from "@prisma/client" ;
+
+export const prismaClient = new PrismaClient();
