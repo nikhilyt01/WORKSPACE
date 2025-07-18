ffee4eeba812ebbf0d368571d44df32566bd591d
nikhilyt01 <nikhil0808yt@gmail.com>
2025-07-18 16:31:11 +0530

    ssr page passes params

---
+++ figma_test/excailRedrawn/apps/web/components/RoomCanvas.tsx
@@ -0,0 +1,12 @@
+import { Canvas } from "./canvas"
+export  function RoomCanvas({roomId}:{roomId:string}){
+    // validate and connect WS 
+    //render unauth and other apage
+
+   return (
+    
+        <Canvas roomId={roomId}></Canvas>
+    
+     
+   )
+}
