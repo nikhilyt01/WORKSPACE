ec58c79eecd69f20bc3c1dde41b850eaa7d9f2cb
nikhilyt01 <nikhil0808yt@gmail.com>
2025-07-20 23:20:47 +0530

    to accept params

---
+++ figma_test/excailRedrawn/apps/web/app/canvas/[roomId]/page.tsx
@@ -0,0 +1,12 @@
+import { Canvas } from "../../../components/canvas";
+import { RoomCanvas } from "../../../components/RoomCanvas";
+export default async function Room({params}:{
+    params:{ roomId:string}
+}){
+  const roomId = (await params).roomId;
+
+  return (
+    <RoomCanvas roomId={roomId}></RoomCanvas>
+  )
+
+}
