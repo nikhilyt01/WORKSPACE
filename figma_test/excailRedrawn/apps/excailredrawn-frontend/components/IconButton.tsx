babb4c765496a50892c4daebe35f1689b8206c06
nikhilyt01 <nikhil0808yt@gmail.com>
2025-07-30 22:35:57 +0530

    ui

--- figma_test/excailRedrawn/apps/excailredrawn-frontend/components/IconButton.tsx
+++ figma_test/excailRedrawn/apps/excailredrawn-frontend/components/IconButton.tsx
@@ -18,7 +18,7 @@ export function IconButton({Icon,Activated,onclick,size}:buttonProps){
     <div className={`rounded-xl p-2 cursor-pointer border  transition-all overflow-hidden duration-200 hover:bg-gray hover:scale-105 active:scale-95
         ${Activated ?"bg-blue-500/50 text-white shadow-lg shadow-blue-500/50 border border-blue-400/50 "
                     :"bg-slate-700/50 text-slate-300 hover:bg-slate-600/50 hover:text-white border border-slate-600/50 hover:border-slate-500/50"}`}  onClick={onclick} >
-       {Icon}
+       {Icon }
     </div>
     )
 
