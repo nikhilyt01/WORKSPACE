0d28ce502590150e28903a4813458253152d6070
nikhilyt01 <nikhil0808yt@gmail.com>
2025-08-09 22:23:46 +0530

    tailwind

--- reduxToolkit/src/redux/store.js
+++ reduxToolkit/src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
 
-export const store = configureStore({
-  reducer: {
-    counter:counterReducer
+export const store = configureStore({     //This approach creates a global state object where each 
+  reducer: {                              //slice's state is nested under a corresponding key.   
+    counter:counterReducer                // global state object {counter:{value:0} }  key:state of slice 
   },
 })
