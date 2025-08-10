233bedce64d5f49dc9f883c5971d53d1eee933dd
nikhilyt01 <nikhil0808yt@gmail.com>
2025-08-10 15:20:52 +0530

    registered multi Reducer

--- reduxToolkit/src/redux/store.js
+++ reduxToolkit/src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
+import todoReducer from '../features/todo/todoSlice'
+
 
 export const store = configureStore({     //This approach creates a global state object where each 
   reducer: {                              //slice's state is nested under a corresponding key.   
-    counter:counterReducer                // global state object {counter:{value:0} }  key:state of slice 
+    counter:counterReducer ,               // global state object {counter:{value:0} }  key:state of slice 
+    todo:todoReducer
   },
 })
