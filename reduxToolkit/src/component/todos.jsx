be5631c445eb44d3dfd375e33b883e2a34a4e35f
nikhilyt01 <nikhil0808yt@gmail.com>
2025-08-17 22:05:40 +0530

    added input after edit btn

--- reduxToolkit/src/component/todos.jsx
+++ reduxToolkit/src/component/todos.jsx
@@ -6,13 +6,15 @@ import { useEffect, useState } from "react";
 
 export function Todos (){
     const [editing,setEditing]=useState(false)
-    useEffect(()=>{
-          console.log(editing)
-    },[editing])
+    const [input,setInput]=useState("");
+    
 
     function handleEdit(){
-    setEditing(true)
-}
+      setEditing(true)
+    }
+    function handleSave(){
+      setEditing(false)
+    }
     const todos= useSelector((state)=>state.todo.todos)
     const dispatch = useDispatch()
 
@@ -20,10 +22,20 @@ export function Todos (){
         <div>
             {todos.map((todo)=>(
               <li className="flex justify-between items-center mt-4 bg-zinc-800 px-4 py-2 rounded " key={todo.id}>
-                <div className="text-white">{todo.text}</div>
+                <div className="text-white">{editing? 
+                  <input 
+                    key={todo.id}
+                    type="text" 
+                    placeholder={todo.text} 
+                    
+                    onChange={(e)=>setInput(e.target.value)}
+                    className="outline-none rounded-lg p-1 hover:ring-2 hover:ring-blue-500"
+                    />
+                  :todo.text}
+                </div>
                 <div className="space-x-2">
                 { editing ? <button className="text-white bg-blue-500 border-0 py-1 px-4 focus:outline-none hover:bg-cyan-600 rounded text-md"
-                              onClick={()=>setEditing(false)}
+                              onClick={handleSave}
                             >
                                 <Save className="w-5 h-5"/>
                             </button>:
