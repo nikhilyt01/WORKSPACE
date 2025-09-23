751e2070104996e5d34ce438aa9ac406d0e291be
nikhilyt01 <nikhil0808yt@gmail.com>
2025-09-24 01:18:34 +0530

    src chnged but build same

--- Docker beigns/index.js
+++ Docker beigns/index.js
@@ -2,7 +2,7 @@ const express =require("express")
 const app=express()
 
 app.get("/",(req,res)=>{
-    res.send("hello world ");
+    res.send("hello world ,src code changed but build was not affected");
 })
 
 app.listen(3000);
