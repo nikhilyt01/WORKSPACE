7cf92dbda524899d685368540d553a59448deed3
nikhilyt01 <nikhil0808yt@gmail.com>
2025-10-19 13:05:32 +0530

    revised router and controller

---
+++ Streamify/backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export async function signup(req,res){
+    res.send("signup endpoint ");
+}
+export async function  login(req,res){
+    res.send("login endpoint ");
+}
+export async function logout(req,res){
+    res.send("logout endpoint ");
+}   
