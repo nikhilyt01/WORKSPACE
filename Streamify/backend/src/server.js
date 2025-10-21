cfd67779f4f6435aea7f472b88b2762dd828c593
nikhilyt01 <nikhil0808yt@gmail.com>
2025-10-21 23:58:47 +0530

    to parse body

--- Streamify/backend/src/server.js
+++ Streamify/backend/src/server.js
@@ -8,17 +8,9 @@ dotenv.config();
 const app =express();
 const PORT=process.env.PORT ;
 
+app.use(express.json());
 app.use("/api/auth",authRoutes);
-//noob way
-// app.get("/api/auth/signup", (req, res) => {
-//     res.send("signup route");
-// });
-// app.get("/api/auth/login", (req, res) => {
-//     res.send("login route");
-// });
-// app.get("/api/auth/logout", (req, res) => {
-//     res.send("logout route");
-// });
+
 app.get("/",(req,res)=>{
     res.send("Welcome to Streamify");
 })
