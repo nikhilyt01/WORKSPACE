d05a82413e13b16fc576505817efec20046f946a
nikhilyt01 <nikhil0808yt@gmail.com>
2025-10-22 18:53:42 +0530

    middleware

---
+++ Streamify/backend/src/middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
+const protectRoute= async (req,res,next)=>{
+    const token=req.cookies.jwt;
+    if(!token){
+        return res.status(401).json({message:"Unauthorized access -token missing"});
+    }
+    try{
+        const decoded=jwt.verify(token,process.env.JWT_SECRET_KEY);
+        if(!decoded){
+            return res.status(401).json({message:"Unauthorized access -invalid token"});
+        }
+
+        const user=await User.findById(decoded.userId).select("-password");
+        if(!user){
+            return res.status(401).json({message:"Unauthorized access -user not found"});
+        }
+
+        req.userId=decoded.userId;//decoded  
+        next();
+    }catch(error){
+        console.log("Error in auth middleware:",error.message);
+    }
+}
