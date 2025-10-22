126187cbcc418377f32002b11bff236d690c482b
nikhilyt01 <nikhil0808yt@gmail.com>
2025-10-22 21:59:05 +0530

    new route for validating user

--- Streamify/backend/src/routes/auth.routes.js
+++ Streamify/backend/src/routes/auth.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { signup,login, logout } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { onboard } from "../controllers/auth.controller.js";
 
 const router= express.Router();
 
@@ -8,6 +10,11 @@ router.post("/signup",signup)
 router.post("/login",login)
 
 router.post("/logout",logout)
-router.post("/onboarding",protectRoute,onboard)
+router.post("/onboarding",protectRoute,onboard);
+
+router.get("/me",protectRoute,(req,res)=>{
+    res.status(200).json({sucess:true,user:req.userId});
+})
+
 
 export default router;
