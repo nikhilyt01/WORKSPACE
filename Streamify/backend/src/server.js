a9532f1bc25605958a73928474523d41b0d03102
nikhilyt01 <nikhil0808yt@gmail.com>
2025-10-23 11:35:29 +0530

    user routes

--- Streamify/backend/src/server.js
+++ Streamify/backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";// bcoz used type : "module" in package.json for import 
 import cookieParser from "cookie-parser";
 import { connectDB } from "./Lib/db.js";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
+app.use("api/users",userRoutes);
 
 app.get("/",(req,res)=>{
     res.send("Welcome to Streamify");
