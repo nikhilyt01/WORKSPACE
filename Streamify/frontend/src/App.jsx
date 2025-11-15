0733f043175ffa51fdea9dbb3f23096a5e7c7823
nikhilyt01 <nikhil0808yt@gmail.com>
2025-11-15 23:45:46 +0530

    added page security

--- Streamify/frontend/src/App.jsx
+++ Streamify/frontend/src/App.jsx
@@ -1,19 +1,42 @@
-import { Route, Routes } from "react-router"
-import SignupPage from "./pages/SignupPage"
+import React from 'react'
+import {Routes,Route} from "react-router"
+import SignupPage from './pages/SignupPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import Callpage from './pages/Callpage.jsx'
+import Loginpage from './pages/Loginpage.jsx'
+import NotificationPage from './pages/NotificationPage.jsx'
+import toast,{Toaster} from "react-hot-toast"
+import { useQuery } from '@tanstack/react-query'
+import axios from  "axios"
+import axiosInsrtance from './lib/axios.js';
+import { Navigate } from 'react-router'
 
 const App = () => {
-  return ( <div className="h-screen " data-theme="night">
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/chat" element={<ChatPage />} />
-      <Route path="/call" element={<Callpage />} /> 
-      <Route path="/notifications" element={<NotificationsPage />} />
-      <Route path="/signup" element={<SignupPage/>} />
-      <Route path="/login" element={<LoginPage/>} />
-      
-    </Routes> 
-  </div>
-   
+  const {data:authData,isLoading,error}=useQuery({
+    queryKey:["authUser"],
+    queryFn:async()=>{
+      const res= await axiosInsrtance.get("/auth/me")
+      return res.data;
+    },
+    retry:false, //auth check only once 
+  })
+  const authUser=authData?.user;
+ 
+
+  return (
+    <div  data-theme="coffee" className="bg-red-500 h-screen  text-5xl">
+     
+      <Routes>
+        <Route path='/' element={authUser?<HomePage/>:<Navigate to="/login"/>}/>
+        <Route  path="/signup" element={!authUser?<SignupPage/>:<Navigate to="/"/>} />
+        <Route path="/login" element={!authUser?<Loginpage/>:<Navigate to="/"/>} />
+        <Route path="/chat" element={authUser?<ChatPage/>:<Navigate to="/login"/>} />
+        <Route path="/call" element={authUser?<Callpage/>:<Navigate to="/login"/>} />
+        <Route path="/notifications" element={authUser?<NotificationPage/>:<Navigate to="/login"/>} /> 
+      </Routes>
+      <Toaster/>
+    </div>
   )
 }
 
