19fe0d0166873386356ea34e2a51a782350c8ee6
nikhilyt01 <nikhil0808yt@gmail.com>
2025-12-21 23:26:23 +0530

    fixed

--- Streamify/frontend/src/hooks/useAuthUser.js
+++ Streamify/frontend/src/hooks/useAuthUser.js
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import axiosInsrtance from "../lib/axios";
-import { authUser } from "../lib/api.js";
+import { getAuthUser } from "../lib/api.js";
 
 export const useAuthUser =() =>{
     const authUser=useQuery({
     queryKey:["authUser"],
-    queryFn:authUser,
+    queryFn:getAuthUser,
     retry:false, //auth check only once 
   })
   return {isLoading:authUser?.isLoading,authUser:authUser.data?.user}
